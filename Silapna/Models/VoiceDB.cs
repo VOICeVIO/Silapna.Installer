4c6e9cf468ac25fcbecd18ab601850101fe4d42a
Ulysses Wu <wdwxy12345@gmail.com>
2023-07-13 01:29:42 +0800

    wip

--- Silapna/Models/VoiceDB.cs
+++ Silapna/Models/VoiceDB.cs
@@ -1,13 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Silapna.Models
 {
     public class VoiceDB
     {
+        public const string BasePath = "Vanarana";
+        public const string Name = "voices.json";
+        public Dictionary<string, List<string>> IdcNameMap { get; set; }
 
+        public async Task<bool> BuildVirtualStorage()
+        {
+            return true;
+        }
+
+        public async Task<bool> DeleteVirtualStorage()
+        {
+            return true;
+        }
     }
 }
--- Silapna/ViewModels/MainViewModel.cs
+++ Silapna/ViewModels/MainViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
 using Silapna.Models;
 
 namespace Silapna.ViewModels;
@@ -41,6 +44,88 @@ public partial class MainViewModel : ViewModelBase
 
     public void LoadVoices()
     {
+
+    }
+
+    private bool CheckIsFreePack(ZipArchive archive)
+    {
+        bool hasPpkg = false;
+        bool hasIdc = false;
+        bool hasSylapack = false;
+
+        foreach (var entry in archive.Entries)
+        {
+            var n = entry.FullName;
+            if (n == "voice.ppkg")
+            {
+                hasPpkg = true;
+            }
+            else if (entry.Name.EndsWith(".sylapack"))
+            {
+                if (!entry.FullName.StartsWith("sylapack"))
+                {
+                    return false;
+                }
+                hasSylapack = true;
+            }
+            else if (entry.Name.EndsWith(".idc"))
+            {
+                if (!entry.FullName.StartsWith("sylapack"))
+                {
+                    return false;
+                }
+                hasIdc = true;
+            }
+        }
+
+        return hasPpkg && hasIdc && hasSylapack;
+    }
+
+    private async Task ConvertToFreePack(ZipArchive archive)
+    {
+
+    }
+
+    public async Task Repack(string path)
+    {
+        if (!File.Exists(path))
+        {
+            HintText = "[Repack] File not found";
+            return;
+        }
+
+        using var fs = File.OpenRead(path);
+        var header = new byte[4];
+        var c = fs.Read(header);
+        if (c < 4)
+        {
+            HintText = "[Repack] File is not valid";
+            return;
+        }
+
+        bool isVppk = true;
+        var newPath = Path.Combine(Path.GetDirectoryName(path)!, Path.GetFileNameWithoutExtension(path) + ".silapna.vppk");
+        
+        if (header[0] == 'P' && header[1] == 'K')
+        {
+            fs.Seek(0, SeekOrigin.Begin);
+            isVppk = false;
+        }
+        try
+        {
+            var archive = new ZipArchive(fs);
+            if (CheckIsFreePack(archive))
+            {
+                if (!isVppk)
+                {
+                    
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            HintText = "[Repack] error: " + e.Message;
+        }
 
     }
 }
